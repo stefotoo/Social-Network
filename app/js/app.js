cf55f846ae8913ca21c8e032634d992bc71f6f22
Stefan Tsekov <stefoto.android@gmail.com>
2015-05-20 22:19:09 +0300

    Material design chosen

---
+++ app/js/app.js
@@ -0,0 +1,25 @@
+var app = angular.module('socialNetworkApp', ['ngMaterial'])
+    .controller('AppCtrl', function($scope, $mdDialog) {
+        $scope.title1 = 'Button';
+        $scope.title4 = 'Warn';
+        $scope.isDisabled = true;
+        $scope.googleUrl = 'http://google.com';
+
+        $scope.alert = '';
+        $scope.showAlert = function(ev) {
+            // Appending dialog to document.body to cover sidenav in docs app
+            // Modal dialogs should fully cover application
+            // to prevent interaction outside of dialog
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .parent(angular.element(document.body))
+                    .title('This is an alert title')
+                    .content('You can specify some description text in here.')
+                    .ariaLabel('Alert Dialog Demo')
+                    .ok('Got it!')
+                    .targetEvent(ev)
+            );
+        };
+
+
+    });
